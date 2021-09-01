539966535133039fa2e1ff8bfd36be9ed7378826
Dino Saciragic <dino@appit.ba>
2021-09-01 17:01:03 +0200

    login and register design done

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,31 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import React, { Fragment } from 'react';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import ContactState from './context/conact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route path='/' exact component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route path='/' exact component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
